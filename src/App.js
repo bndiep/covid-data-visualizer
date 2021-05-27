ebd1c5aeae97de9eb51ae6b085eaa7041c286d4f
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-05-26 19:09:47 -0700

    initial commit with Nivo installed

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import GraphContainer from './components/GraphContainer'
+
+import './App.css'
+import GraphContainer from './components/GraphContainer'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello World.</h1>
+      <GraphContainer />
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/GraphContainer.js
@@ -0,0 +1,9 @@
+const GraphContainer = () => {
+  return (
+    <React.Fragment>
+      <h1>Vaccinated Population in US</h1>
+    </React.Fragment>
+  )
+}
+
+export default GraphContainer
