65008a093cba4bb52d25ab43bed89a703767f6b0
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-06-03 13:09:56 -0700

    displays US vaccination data from one fetch call

--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -2,29 +2,39 @@ import { useState, useEffect } from 'react'
 
 const GraphContainer = () => {
   const [administered, setAdministered] = useState(0)
+  const [vaccinatedPopulation, setVaccinatedPopulation] = useState(0)
+  const [partiallyVaccinated, setPartiallyVaccinated] = useState(0)
+  const [totalPopulation, setTotalPopulation] = useState(0)
+  // async always returns a Promise
+  // asycn is a prefix wrapper for Promises
   const showVaccinePopulation = async () => {
+    // use await to wait for the Promise to resolve
     let response = await fetch('https://covid-api.mmediagroup.fr/v1/vaccines?country=US')
     let vaccinationData = response.json()
-    console.log('vaccination data', vaccinationData)
+    // don't forget to return the data!
     return vaccinationData
   }
 
   useEffect(() => {
     showVaccinePopulation()
     .then(data => {
-      console.log('data', data['All']['administered'])
       setAdministered(data['All']['administered'])
+      setVaccinatedPopulation(data['All']['people_vaccinated'])
+      setPartiallyVaccinated(data['All']['people_partially_vaccinated'])
+      setTotalPopulation(data['All']['population'])
     })
     .catch(error => {
       console.log('error', error)
     })
   }, [])
 
-  console.log('administered', administered)
   return (
     <>
       <h1>Vaccinated Population in US</h1>
-      {/* <p>{data}</p> */}
+      <p>Population: {totalPopulation}</p>
+      <p>Administered: {administered}</p>
+      <p>Number of People Vaccinateed: {vaccinatedPopulation}</p>
+      <p>Number of People Partially Vaccinated: {partiallyVaccinated}</p>
     </>
   )
 }
