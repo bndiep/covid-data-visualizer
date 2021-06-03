fe123649d6084fee563c173e5a5aef34550ea5a0
bachdiep <29311886+bndiep@users.noreply.github.com>
2021-06-03 12:50:38 -0700

    fetches data using async await

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import GraphContainer from './components/GraphContainer'
-
 import './App.css'
-import GraphContainer from './components/GraphContainer'
 
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>Hello World.</h1>
       <GraphContainer />
-    </React.Fragment>
+    </>
   )
 }
 
--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react'
+
 const GraphContainer = () => {
+  const [administered, setAdministered] = useState(0)
+  const showVaccinePopulation = async () => {
+    let response = await fetch('https://covid-api.mmediagroup.fr/v1/vaccines?country=US')
+    let vaccinationData = response.json()
+    console.log('vaccination data', vaccinationData)
+    return vaccinationData
+  }
+
+  useEffect(() => {
+    showVaccinePopulation()
+    .then(data => {
+      console.log('data', data['All']['administered'])
+      setAdministered(data['All']['administered'])
+    })
+    .catch(error => {
+      console.log('error', error)
+    })
+  }, [])
+
+  console.log('administered', administered)
   return (
-    <React.Fragment>
+    <>
       <h1>Vaccinated Population in US</h1>
-    </React.Fragment>
+      {/* <p>{data}</p> */}
+    </>
   )
 }
 
